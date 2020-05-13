9781b8ea063a62917fa83f1c3c4583b423360656
Tara McDonagh <Taramcdonagh@hotmail.com>
2020-05-13 10:24:37 +0100

    Step 3

--- TaraMcDonagh_S00185883/Phone.cs
+++ TaraMcDonagh_S00185883/Phone.cs
@@ -6,14 +6,20 @@ using System.Threading.Tasks;
 
 namespace TaraMcDonagh_S00185883
 {
-    class Phone
+    public class Phone
     {
         public string Name { get; set; }
         public int Price { get; set; }
         public string OperatingSystem { get; set; }
         public string OS_Image { get; set; }
         public string Phone_Image { get; set; }
+        public double IncreasePrice()
+        {
+            double percentage;
+            double IncreasePrice = (Price*percentage) + Price;
 
+            return IncreasePrice;
+        }
 
     }
 }

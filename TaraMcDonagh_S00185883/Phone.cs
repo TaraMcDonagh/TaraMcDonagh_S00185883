785e2cbd0a9a9cf3f27af3e8a2de09e95510c846
Tara McDonagh <Taramcdonagh@hotmail.com>
2020-05-13 10:11:44 +0100

    Step 2

---
+++ TaraMcDonagh_S00185883/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaraMcDonagh_S00185883
+{
+    class Phone
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+
+
+    }
+}
